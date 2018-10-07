62fd7c137b10490e5bae1e5d9c0192f95eee7003
yuan3675 <r06922152@ntu.edu.tw>
2018-10-07 14:36:26 +0800

    Add hw3

---
+++ hw3/hw3.py
@@ -0,0 +1,28 @@
+from PIL import Image
+
+image = Image.open('lena.bmp')
+width, height = image.size
+pixels = image.load()
+
+imageCopy = Image.new('L', (width, height), 'white')
+
+# calculate histogram
+histogram = [0] * 256
+for i in range(0, width):
+	for j in range(0, height):
+		histogram[pixels[i, j]] += 1
+
+# equalization function
+S = [0] * 256
+totalPixels = width * height
+for i in range(len(histogram)):
+	for j in range(i):
+		S[i] = S[i] + (histogram[j] / totalPixels)
+	S[i] = (int) (S[i] * 255)
+
+# put S value into image
+for i in range(0, width):
+	for j in range(0, height):
+		imageCopy.putpixel((i, j), S[pixels[i, j]])
+
+imageCopy.save('HistogramEqualization.bmp')
