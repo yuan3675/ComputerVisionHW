bfc4ec7c1477d6cc71c63c89c426be23ac27c46b
yuan3675 <r06922152@ntu.edu.tw>
2018-12-04 00:48:49 +0800

    add difference of gaussian

--- hw10/hw10.py
+++ hw10/hw10.py
@@ -63,7 +63,7 @@ def MinimumVariance(image, threshold):
 def LaplaceOfGaussian(image, threshold):
     width, height = image.size
     pixels = image.load()
-    tmp = Image.new('L', (width+10, height+10), 'black')
+    tmp = Image.new('L', (width+10, height+10), 0)
     output = Image.new('1', (width, height), 1)
     mask = [[0, 0, 0, -1, -1, -2, -1, -1, 0, 0, 0], [0, 0, -2, -4, -8, -9, -8, -4, -2, 0, 0],
     [0, -2, -7, -15, -22, -23, -22, -15, -7, -2, 0], [-1, -4, -15, -24, -14, -1, -14, -24, -15, -4, -1],
@@ -87,6 +87,33 @@ def LaplaceOfGaussian(image, threshold):
                 output.putpixel((i-5, j-5), 0)
     return output
 
+def DifferenceOfGaussian(image, threshold):
+    width, height = image.size
+    pixels = image.load()
+    tmp = Image.new('L', (width+10, height+10), 'black')
+    output = Image.new('1', (width, height), 0)
+    mask = [[-1, -3, -4, -6, -7, -8, -7, -6, -4, -3, -1], [-3, -5, -8, -11, -13, -13, -13, -11, -8, -5, -3],
+    [-4, -8, -12, -16, -17, -17, -17, -16, -12, -8, -4], [-6, -11, -16, -16, 0, 15, 0, -16, -16, -11, -6],
+    [-7, -13, -17, 0, 85, 160, 85, 0, -17, -13, -7], [-8, -13, -17, 15, 160, 283, 160, 15, -17, -13, -8],
+    [-7, -13, -17, 0, 85, 160, 85, 0, -17, -13, -7], [-6, -11, -16, -16, 0, 15, 0, -16, -16, -11, -6],
+    [-4, -8, -12, -16, -17, -17, -17, -16, -12, -8, -4], [-3, -5, -8, -11, -13, -13, -13, -11, -8, -5, -3],
+    [-1, -3, -4, -6, -7, -8, -7, -6, -4, -3, -1]]
+    
+    for j in range(5, height+5):
+        for i in range(5, width+5):
+            tmp.putpixel((i, j), pixels[i-5, j-5])
+
+    padding = tmp.load()
+    for j in range(5, height+5):
+        for i in range(5, width+5):
+            result = 0
+            for m in range(11):
+                for n in range(11):
+                    result += padding[i+n-5, j+m-5] * mask[m][n] 
+            if result > threshold:
+                output.putpixel((i-5, j-5), 1)
+    return output
+
 # main function
 image = Image.open('lena.bmp').convert('L')
 mask1 = LaplaceMask1(image, 15)
@@ -95,5 +122,7 @@ mask2 = LaplaceMask2(image, 15)
 mask2.save('Mask2.bmp')
 minVariance = MinimumVariance(image, 20)
 minVariance.save('MinimumVariance.bmp')
-laplaceGaussian = LaplaceOfGaussian(image, 300)
+laplaceGaussian = LaplaceOfGaussian(image, 3000)
 laplaceGaussian.save('LaplaceOfGaussian.bmp')
+differenceGaussian = DifferenceOfGaussian(image, 1)
+differenceGaussian.save('DifferenceOfGaussian.bmp')
